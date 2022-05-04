89c25cffa8fa9b29c4b1b42d42d977983de4e0b2
urlastway <andriy.semenchenko13@gmail.com>
2022-05-04 23:29:07 +0200

    sneck6

---
+++ snack6/main.js
@@ -0,0 +1,9 @@
+const number = Number(prompt("inserisci il numero"));
+
+if (number % 2==0){
+    console.log(number)
+}
+else{
+    const number = Number(prompt("inserisci il numero"));
+    console.log(number)
+}
