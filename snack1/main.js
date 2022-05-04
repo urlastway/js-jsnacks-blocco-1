9c37709c229b3a8474beb79291d8267d94771a87
urlastway <andriy.semenchenko13@gmail.com>
2022-05-04 11:33:22 +0200

    first commit

---
+++ snack1/main.js
@@ -0,0 +1,10 @@
+const number1 = Number(prompt("Inserisci il numero1"));
+const number2 = Number(prompt("Inserisci il numero1"));
+
+if(number1 > number2){
+    console.log(number1)
+}
+
+else if(number1 < number2){
+    console.log(number2)
+}
