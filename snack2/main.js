e70f2f1d9fa655f4f1a6111af15c64b6b9000649
urlastway <andriy.semenchenko13@gmail.com>
2022-05-04 11:59:03 +0200

    com

--- snack2/main.js
+++ snack2/main.js
@@ -2,10 +2,10 @@ const parola1 = String(prompt("inserisci la prima parola"))
 const parola2 = String(prompt("inserisci la seconda parola"))
 
 if (parola1.length > parola2.length){
-    console.log(parola1)
+    console.log(parola1, parola2)
 }
 else if (parola1.length < parola2.length){
-    console.log(parola2)
+    console.log(parola2, parola1) 
 }
 
 else{
